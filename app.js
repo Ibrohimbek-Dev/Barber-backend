0521ef87b840406e2a70adb46a56e00d06e2b8ff
IbraheemBek Developer <programming012@gmail.com>
2023-08-17 11:32:18 +0900

    make it better

--- app.js
+++ app.js
@@ -4,62 +4,61 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
-const mongoURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nqsvctw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-const HttpError = require("./models/http-error");
-
 const app = express();
 
 app.use(bodyParser.json());
 
+// Serve static files
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 app.use(express.static(path.join("public")));
 
-
-// app.use((req, res, next) => {
-// 	res.setHeader("Access-Control-Allow-Origin", "*");
-// 	res.setHeader(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-// 	);
-// 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-// 	next();
-// });
+// Routes
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
-app.use((req, res, next) => {
-	res.sendFile(path.resolve(__dirname, "public", "index.html"))
-})
+// Serve React frontend
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "frontend", "build")));
 
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+	});
+}
 
-// app.use((req, res, next) => {
-// 	const error = new HttpError("Could not find this route.", 404);
-// 	throw error;
-// });
-
+// Error handling middleware
 app.use((error, req, res, next) => {
 	if (req.file) {
 		fs.unlink(req.file.path, (err) => {
-			console.log("app.js: ", err);
+			console.log("Error cleaning up file:", err);
 		});
 	}
-	if (res.headerSent) {
+
+	if (res.headersSent) {
 		return next(error);
 	}
 
-	res.status(error.code || 500);
-	res.json({ message: error.message || "An unknown error occured!" });
+	res
+		.status(error.code || 500)
+		.json({ message: error.message || "An unknown error occurred!" });
 });
 
+// Database connection and server start
 mongoose
-	.connect(mongoURL)
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
 	.then(() => {
-		app.listen(process.env.PORT || 5000);
+		console.log("Connected to MongoDB");
+		const port = process.env.PORT || 5000;
+		app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
 	})
 	.catch((error) => {
-		console.log(error);
+		console.error("MongoDB connection error:", error);
 	});
